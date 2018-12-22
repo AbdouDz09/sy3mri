fff5ac88d7ebf62454277102cff6e6b4bb41581e
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2018-12-22 18:53:02 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -183,7 +183,7 @@ m.sendMessage(args)
       console.log(`ON ${client.guilds.size} Servers '     Script By : abdou-DZ ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`d!help Dream !! ,`,"http://twitch.tv/abdoufersaoui_dz")
+client.user.setGame(`&help Abdou !! ,`,"http://twitch.tv/abdoufersaoui_dz")
 client.user.setStatus("Dz")
 });
 
